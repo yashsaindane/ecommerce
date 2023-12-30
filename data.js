8f5e1c133ae006b2707a837f05933ce5e9cc5db5
yashsaindane <yashsaindane453@gmail.com>
2023-12-30 16:20:10 +0530

    Add filename.txt

---
+++ data.js
@@ -0,0 +1,177 @@
+//Iphone 15
+{
+   // image:"main.jpg" ,
+    pname:"APPLE iPhone 15 Pro Max (Blue Titanium, 256 GB)",
+    subName:"iPhone 15 Pro Max",
+    rate:{star:4.7,count:271},
+    price:159900,
+    oprice:156900,
+    discount:1
+
+}
+//Nothing Phone 2
+{
+    image:"Nothing phone 2(Main).jpg" ,
+    pname:"Nothing Phone (2) (Dark Grey, 512 GB)  (12 GB RAM)",
+    subName:"Phone_2",
+    rate:{star:4.4,count:9009},
+    price:59999,
+    oprice:45999,
+    discount:23
+
+}
+//Samsung Galaxy S23
+{
+    image:"Samsung Galaxy S23(Main).jpg" ,
+    pname:"SAMSUNG Galaxy S23 Ultra 5G (Green, 256 GB)  (12 GB RAM)",
+    subName:"S23 Ultra",
+    rate:{star:4.6,count:2623},
+    price:149999,
+    oprice:124999,
+    discount:16
+
+}
+//vivo V27
+{
+    image:"vivo v27(main).jpg" ,
+    pname:"vivo V27 5G (Magic Blue, 256 GB)  (12 GB RAM)",
+    subName:"V27",
+    rate:{star:4.4,count:3458},
+    price:40999,
+    oprice:36999,
+    discount:9
+
+}
+//realme11
+{
+    image:"Realme 11 Pro(main).jpg" ,
+    pname:"realme 11 Pro 5G (Sunrise Beige, 256 GB)  (12 GB RAM)",
+    subName:"realme 11 Pro",
+    rate:{star:4.3,count:1763},
+    price:30999,
+    oprice:27999,
+    discount:9
+
+}
+//OPPO A78
+{
+    image:"OPPO A78(main).jpg" ,
+    pname:"OPPO A78 (Aqua Green, 128 GB)  (8 GB RAM)#JustHere",
+    subName:"A78",
+    rate:{star:4.4,count:393},
+    price:22999,
+    oprice:15499,
+    discount:32
+
+}
+//Google pixel 8
+{
+    image:"Google Pixel 8 pro(main).jpg" ,
+    pname:"Google Pixel 8 Pro (Bay, 128 GB)  (12 GB RAM)#JustHere",
+    subName:"Pixel 8 Pro",
+    rate:{star:4.4,count:526},
+    price:106999,
+    oprice:106999,
+    discount:0
+
+}
+//Redmi Note 13
+{
+    image:"Readmi 12 Pro+(main).jpg" ,
+    pname:"REDMI Note 12 Pro+ 5G (Iceberg Blue, 256 GB)  (8 GB RAM)",
+    subName:"Note 12 Pro+",
+    rate:{star:4.2,count:18735},
+    price:33999,
+    oprice:27999,
+    discount:17
+
+}
+//Motorola Edge 40
+{
+    image:"Motorola Edge 40(main).jpg" ,
+    pname:"MOTOROLA Edge 40 Neo (Soothing Sea, 128 GB)  (8 GB RAM)",
+    subName:"Motorola edge 40",
+    rate:{star:4.4,count:17178},
+    price:27999,
+    oprice:22999,
+    discount:17
+
+}
+//Samsung Galaxy S21 FE
+{
+    image:"Samsung Galaxy S23(Main).jpg" ,
+    pname:"Samsung Galaxy S21 FE 5G with Snapdragon 888 (Navy, 128 GB)  (8 GB RAM)#JustHere",
+    subName:"S23",
+    rate:{star:4.3,count:121792},
+    price:69999,
+    oprice:32999,
+    discount:52
+
+}
+//OnePlus 11R 5G (Galactic Silver, 128 GB)  (8 GB RAM)
+{
+    image:"OnePlus 11R(main).jpg" ,
+    pname:"OnePlus 11R 5G (Galactic Silver, 128 GB)  (8 GB RAM)",
+    subName:"OnePlus 11R",
+    rate:{star:4.5,count:10383},
+    price:39999,
+    oprice:39999,
+    discount:0
+
+}
+//iPhone 14 Plus(main)
+{
+    image:"iPhone 14 Plus(main).jpg" ,
+    pname:"APPLE iPhone 14 Plus (Midnight, 128 GB)",
+    subName:"iPhone 14 Plus",
+    rate:{star:4.6,count:57332},
+    price:79900,
+    oprice:65999,
+    discount:17
+
+}
+//POCO-m6
+{
+    image:"POCO-M6-5G-Galactic-Black.jpg" ,
+    pname:"POCO M6 5G (Orion Blue, 128 GB)  (4 GB RAM)",
+    subName:"POCO M6",
+    rate:{star:4.1,count:35},
+    price:12999,
+    oprice:10499,
+    discount:19
+
+}
+//Vivo X90 Pro
+{
+    image:"Vivo X90 Pro.jpg" ,
+    pname:"vivo X90 Pro (Legendary Black, 256 GB)  (12 GB RAM)",
+    subName:"POCO M6",
+    rate:{star:4.4,count:685},
+    price:91999,
+    oprice:69799,
+    discount:24
+
+}
+//OnePlus Nord CE3 5G 
+{
+    image:"oneplus-nord-ce3-5g.jpg" ,
+    pname:"OnePlus Nord CE3 5G (Grey Shimmer, 128 GB)  (8 GB RAM)",
+    subName:"OnePlus Nord CE3 5G",
+    rate:{star:4.4,count:3617},
+    price:26999,
+    oprice:24945,
+    discount:7
+
+}
+// laptops 
+//MSI Katana 17 Intel Core i7 12th Gen 12650H 
+{
+    image:"MSI Katana 17 b12V.jpg" ,
+    pname:"MSI Katana 17 Intel Core i7 12th Gen 12650H ",
+    subName:"OnePlus Nord CE3 5G",
+    rate:{star:4.4,count:3617},
+    price:26999,
+    oprice:24945,
+    discount:7
+
+}
